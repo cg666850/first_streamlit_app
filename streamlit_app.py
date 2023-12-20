aae4e13773b9773e8886548cd87a78332044e605
cg666850 <154053125+cg666850@users.noreply.github.com>
2023-12-20 13:53:46 +0530

    Update streamlit_app.py
    fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # response with table form 
 streamlit.dataframe(fruityvice_normalized)
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+
