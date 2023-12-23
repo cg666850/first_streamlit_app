ab3443691781ea448a6281d5f97f6eb8b3df5913
cg666850 <154053125+cg666850@users.noreply.github.com>
2023-12-23 13:26:41 +0530

    Update streamlit_app.py
    Format correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,8 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 # streamlit.text("Hello from Snowflake:")
-streamlit.text("My fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.header("My fruit load list contains:")
+streamlit.dataframe(my_data_row)
 
 
 
