24a95b52a8bc79ea476a129bdb1854177bec5702
cg666850 <154053125+cg666850@users.noreply.github.com>
2023-12-23 15:30:50 +0530

    Update streamlit_app.py
    Error function add

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,21 +26,27 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  streamlit.error("Please select fruit to get information.")
+  else
+  # friutyvice response added to the pandas libarary
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# response with table form 
+streamlit.dataframe(fruityvice_normalized)
 
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+except URLError as e:
+streamlit.error()
 streamlit.write('The user entered ', fruit_choice)
 
-streamlit.header("Fruityvice Fruit Advice!")
+
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 
-# friutyvice response added to the pandas libarary
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# response with table form 
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 #import requests
