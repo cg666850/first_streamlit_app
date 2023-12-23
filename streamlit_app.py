1d1e76e3541f6f015e4d54c044e0869bfee08fd7
cg666850 <154053125+cg666850@users.noreply.github.com>
2023-12-23 15:53:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,14 +20,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
-
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
 
 # New section to display aPI response
 streamlit.header("Fruityvice Fruit Advice!")
-try:
 fruit_choice=streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
   streamlit.error("Please select a fruit to get information.")
