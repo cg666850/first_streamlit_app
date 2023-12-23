31d25da9afd4b34a609edb6964d95339e6d95839
cg666850 <154053125+cg666850@users.noreply.github.com>
2023-12-23 15:45:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,11 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+
+# New section to display aPI response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input("What fruit would you like information about?")
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
   streamlit.error("Please select fruit to get information.")
   else
